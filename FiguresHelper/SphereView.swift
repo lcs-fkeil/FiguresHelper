e1ee41f4aa5632e01af89189c2290ef267efc576
Franka Keil <fkeil@lcs.on.ca>
2021-12-01 10:59:02 -0500

    Added interactive Sphere View

--- FiguresHelper/SphereView.swift
+++ FiguresHelper/SphereView.swift
@@ -1,5 +1,5 @@
 //
-//  PreparedCircleView.swift
+//  SphereView.swift
 //  FiguresHelper
 //
 //  Created by Franka Keil on 2021-12-01.
@@ -7,49 +7,38 @@
 
 import SwiftUI
 
-struct CircleView: View {
+struct SphereView: View {
     
     // MARK: Stored properties
-    
-    // "radius" is the name
-    // "Double" is the data type
-    // By adding = 15.00 we are just providing a default value.
-    // Input - we need this from the user
-    // We want SwiftUI to update the user interface based on what the user selects.
-    // To do this, we use a special piece of syntax called a "property wrapper2
-    //
-    // @State
-    //
-    //This marks property as being one that SwiftUI should "watch" for changes
     @State var radius: Double = 15.00
     
     // MARK: Computed properties
+    var area: Double{
+        return 4 * Double.pi * radius * radius
+    }
     
-    var area: Double {
-        return Double.pi * radius * radius
+    var volume: Double{
+        return 4/3 * Double.pi * radius * radius * radius
     }
     
     // User interface
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
+        VStack(alignment: .leading, spacing: 20){
             
-            // Input
-            Text("Radius:")
+            Text("radius")
                 .bold()
             
-            Group {
+            Group{
                 
-                // Show the selected radius value
-                HStack {
+                HStack{
                     Spacer()
                     Text("\(radius)")
                         .font(.title2)
                         .bold()
                     Spacer()
+                    
                 }
                 
-                // The syntax of $ says to use this property (radius) and BIND IT to this control
-                // To "bind" means that when the control changes, the properties value changes
                 Slider(value: $radius,
                        in: 0.0...100.0,
                        step: 1.0,
@@ -62,30 +51,32 @@ struct CircleView: View {
                        maximumValueLabel: {
                     Text("100.0")
                 })
-                
             }
             
-            
-            // Output
             Text("Area:")
                 .bold()
             
             Text("\(area) square units")
                 .font(.title2)
             
+            Text("Volume: ")
+                .bold()
+            
+            Text("\(volume) cubed units")
+                .font(.title2)
+            
             
             Spacer()
-            
         }
         .padding()
-        .navigationTitle("Circle")
+        .navigationTitle("Sphere")
     }
 }
 
-struct CircleView_Previews: PreviewProvider {
+struct SphereView_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationView {
-            CircleView()
+        NavigationView{
+            SphereView()
         }
     }
 }
